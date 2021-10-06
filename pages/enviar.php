b62ec070d31efffa51e03b89e6bc099086978a52
Luciano Nunez <lucianonahuelnunez@gmail.com>
2021-10-06 19:53:57 -0300

    mejoras formulario

--- pages/enviar.php
+++ pages/enviar.php
@@ -1,11 +1,10 @@
 <?php
  $destino= "lucho.n2102@gmail.com"
- $nombre= $_Post["nombre"];
- $nombre= $_Post["email"];
- $nombre= $_Post["telefono"];
- $nombre= $_Post["mensaje"];
+ $nombre = $_Post["nombre"];
+ $email = $_Post["email"];
+ $telefono = $_Post["telefono"];
+ $mensaje = $_Post["mensaje"];
  $contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono" . $telefono . "\nMensaje" . $mensaje;
  mail($destino,"Contacto", $contenido);
- 
-
+ header("Location:gracias.html");
 ?>
